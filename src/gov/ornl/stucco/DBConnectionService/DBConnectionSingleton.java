9fd22e3fcc74b41596089e4f330943b9e6208a11
Maria Vincent <mvincen2@vols.utk.edu>
2017-03-21 13:28:55 -0400

    added elasticsearch db connection instantiation

--- src/gov/ornl/stucco/DBConnectionService/DBConnectionSingleton.java
+++ src/gov/ornl/stucco/DBConnectionService/DBConnectionSingleton.java
@@ -3,9 +3,15 @@ package gov.ornl.stucco.DBConnectionService;
 import gov.pnnl.stucco.dbconnect.inmemory.InMemoryDBConnection;
 import gov.pnnl.stucco.dbconnect.DBConnectionAlignment;
 import gov.pnnl.stucco.dbconnect.DBConnectionFactory;
+import gov.pnnl.stucco.dbconnect.elasticsearch.ElasticsearchDBConnection;
+import gov.pnnl.stucco.dbconnect.elasticsearch.Connection;
 
 import javax.inject.Singleton;
 import javax.ws.rs.Path;
+
+import java.net.UnknownHostException;
+
+import java.io.FileNotFoundException;
  
 /**
  * This singleton will maintain a persistent DBConnection object, which will be accessible when servicing requests.
@@ -19,6 +25,9 @@ public class DBConnectionSingleton {
 
     private DBConnectionFactory factory;
     private DBConnectionAlignment db;
+
+    private ElasticsearchDBConnection es;
+    private Connection connection;
 
     public DBConnectionSingleton() {
         System.out.println("CREATING SINGLETON!!");
@@ -36,11 +45,30 @@ public class DBConnectionSingleton {
         factory.setConfiguration(config);
         db = factory.getDBConnectionTestInterface();
 
+        config = System.getenv("SITU_DB_CONFIG");
+        if (config == null) {
+            throw (new NullPointerException("Missing environment variable SITU_DB_CONFIG"));
+        }
+
+        try {
+            es = new ElasticsearchDBConnection(config); 
+            connection = es.getConnection();
+            connection.open();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("SINGLETON WAS CREATED!!");
     }
 
     public DBConnectionAlignment getDB() {
         return db;
+    }
+
+    public Connection getES() throws UnknownHostException {
+        return connection;
     }
 
 }
